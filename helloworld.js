60d37a7ae45404adb418161a338322dd793bdcf1
nookalamaryville <59845939+nookalamaryville@users.noreply.github.com>
2020-09-08 02:43:32 -0700

    Update helloworld.js
    changed message

--- helloworld.js
+++ helloworld.js
@@ -7,7 +7,7 @@ http.createServer(function (request, response) {
    response.writeHead(200, {'Content-Type': 'text/plain'})
 
    // Send the response body as "Hello World"
-   response.end('Hello World\n')
+   response.end('Welcome to Week2 Home Work.\n')
 }).listen(3000)
 
 // Console will print the message
